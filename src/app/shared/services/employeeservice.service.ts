0ca2e9a7c03f4106bd21d5dabb8dd75140a62495
Mr.linH <mr.linh1090@gmail.com>
2019-08-17 13:20:18 +0700

    add module auth, pagenotfound

---
+++ src/app/shared/services/employeeservice.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Employee } from '../defines/employee';
+import { Loginemployee } from '../defines/loginemployee';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EmployeeserviceService {
+
+
+  url = "http://localhost/";
+
+  constructor(private http: HttpClient) { }
+
+  createemployee(employee: Employee): Observable<any> {
+
+    return this.http.post(this.url + 'api/employeemasters', employee)
+  }
+  
+  loginemployee(loginEmployee: Loginemployee): Observable<any> {
+    return this.http.post(this.url + 'api/login', loginEmployee)
+  }
+}
